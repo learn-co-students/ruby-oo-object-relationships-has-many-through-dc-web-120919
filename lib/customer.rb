512b1584392556c30106d2fe02a223337932e55d
John Louis Rumingan <jmrumingan@gmail.com>
2019-12-13 15:34:46 -0500

    Fixed best tip enum

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,36 @@
+require "pry"
+
 class Customer
   
+    attr_reader :name
+    attr_writer :age 
+
+    @@all = []
+
+    def initialize(name, age)
+
+        @name = name
+        @age = age
+        @@all << self 
+    end 
+
+    def self.all
+        
+        return @@all
+    end 
+
+    def new_meal(waiter, total, tip)
+
+        Meal.new(waiter, self, total, tip)
+    end 
+
+    def meals
+
+        Meal.all.select {|meal|meal.customer == self }
+    end 
+
+    def waiters 
+
+        self.meals.map {|meal|meal.waiter}
+    end 
 end
